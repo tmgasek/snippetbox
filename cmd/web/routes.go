e5f2b94b324138fa755a82b8016f11791b92e22b
tmgasek <tomaszgasek28@gmail.com>
2023-09-17 16:53:56 +0100

    e2e test

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,8 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.FS(ui.Files))
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
+
 	// For dynamic routes.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
