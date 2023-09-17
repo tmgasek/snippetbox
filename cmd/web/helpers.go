edcaf020b856aec40df7cd1eac122d4a70a5838a
tmgasek <tomaszgasek28@gmail.com>
2023-09-17 11:45:06 +0100

    auth csrf F

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 // write error msg and stack trace to errorLog, send generic 500 res to user
@@ -70,6 +71,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
@@ -98,5 +100,10 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 
 // Return true if curr req is coming from an authenticated user, else false.
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
 }
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 // secureHeaders -> servemux -> app handler
@@ -64,6 +67,47 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		w.Header().Add("Cache-Control", "no-store")
 
 		// Call next handler in chain.
+		next.ServeHTTP(w, r)
+	})
+}
+
+// NoSurf middleware func which uses a customised CSRF cookie with the Secure,
+// Path, and HttpOnly attributes set.
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get value from session. Will return zero value for int (0) if no
+		// such value in session. - Call next handler in chain and return.
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Check if user with that ID exists
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		// If exists, we know that the req is coming from authed user in DB.
+		// Create a new copy of req with extra ctx val, and assign to r.
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
